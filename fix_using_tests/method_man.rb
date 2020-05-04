e68860ecc103019b1eb063e97f4ccd8597d90f87
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 19:15:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,7 +2,7 @@ require 'pry'
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
