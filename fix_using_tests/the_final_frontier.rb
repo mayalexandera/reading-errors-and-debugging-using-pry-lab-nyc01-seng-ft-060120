149222000299c43748b813748156f511ceea3fbf
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 19:22:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,12 +2,12 @@ require 'pry'
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  return (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  "Captain's Log, star date #{star_date}.to_s."
 end
 
 def engage
