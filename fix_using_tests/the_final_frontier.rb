ce7f442c3bed2b48184dee58c846e484faee583a
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 19:22:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,10 +7,11 @@ end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date.to_s}."
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
+  
 end
